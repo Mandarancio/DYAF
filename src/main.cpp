c2bfb78a8698fdb535404259498570c8990d7f07
Martino Ferrari <manda.mgf@gmail.com>
2018-01-22 15:18:17 +0100

    updated testcode

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,15 @@
 #include <string>
 
 #include "dyaf.h"
-#include "scidown.h"
+#include "extensions/scidown.h"
 #include <regex>
 
 int main() {
   std::cout << "Do Yourself A Favor!\n";
   
   // Example of AST generation.
-  std::string code = "# Title\nAnother _line_...\n## A sub-chapter\netc *etc* etc...\n# A Second chapter\nthis is ~not~ a [link](url).\n Bla -bla- bla...\nThis @(ref1) is a reference to something not yet referenced.\n";
+  std::string code = "# Title\nAnother _line_...\n## A sub-chapter\netc *etc* etc...\n# A Second chapter\nthis is ~not~ a [link](url).\n Bla -bla- bla...\nThis @(ref1) is a reference to something not yet referenced.\nThis is an image:\n![the caption](image_url @ref)\n";
 
   auto parser = dyaf::Parser(scidown::load_extension());
-  
   std::cout<<parser.parse(code)->to_str()<<"\n";
-
 }
