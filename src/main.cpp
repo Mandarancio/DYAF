966d183947f903bb3fb5d1df18e37b5f1e7b8b14
Martino Ferrari <manda.mgf@gmail.com>
2018-01-22 16:56:53 +0100

    some more cases

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,20 @@
 
 #include "dyaf.h"
 #include "extensions/scidown.h"
+#include "extensions/math.h"
 #include <regex>
 
 int main() {
   std::cout << "Do Yourself A Favor!\n";
   
   // Example of AST generation.
-  std::string code = "# Title\nAnother _line_...\n## A sub-chapter\netc *etc* etc...\n# A Second chapter\nthis is ~not~ a [link](url).\n Bla -bla- bla...\nThis @(ref1) is a reference to something not yet referenced.\nThis is an image:\n![the caption](image_url @ref)\n";
+  std::string code = "# Title\nAnother _line_...\n## A sub-chapter\netc *etc* etc...\n# A Second chapter\nthis is ~not~ a [link](url).\n Bla -bla- bla...\nThis @(ref1) is a reference to something not yet referenced.\nThis is an image:\n![the caption](image_url @ref size{30, 30})\n";
 
   auto parser = dyaf::Parser(scidown::load_extension());
   std::cout<<parser.parse(code)->to_str()<<"\n";
+  
+  code = "21.2*3+2";
+  parser = dyaf::Parser(math::load_extension());
+  std::cout<<parser.parse(code)->to_str()<<"\n";
+  
 }
